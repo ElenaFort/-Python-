41306894689ab808d2fa599becad0500a93a5ef3
Elena <fortygina.lena@mail.ru>
2022-12-27 02:39:45 +0500

    3_lesson 1_zadacha Format

--- 3_lesson/3_1.py
+++ 3_lesson/3_1.py
@@ -1,4 +1,4 @@
-# 1. Задайте список, состоящий из произвольных чисел, количество 
+# 1. Задайте список, состоящий из произвольных чисел, количество
 # задаёт пользователь.
 # Напишите программу, которая найдёт сумму элементов списка,
 # стоящих на нечётных позициях(не индексах).
@@ -16,15 +16,21 @@
 # [4, 2, 4, 9]
 # 8
 from random import sample
+
+
 def num_find(len_list):
-    num_list = sample (range(1, len_list * 3), k = len_list)
+    num_list = sample(range(1, len_list * 3), k=len_list)
     return num_list
+
+
 def honesty_position(num_list):
     summ = 0
     for i in range(len(num_list)):
         if i % 2 == 0:
             summ = summ + num_list[i]
     print(summ)
+
+
 num_list = num_find(int(input('Введите количество элементов в списке: ')))
 print(num_list)
-honesty_position (num_list)
+honesty_position(num_list)
